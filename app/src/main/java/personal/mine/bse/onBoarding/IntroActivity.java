df92ff5d34c6075f05895cbfb609a633315b8158
daffaghfr <58648237+daffaghfr@users.noreply.github.com>
2020-10-02 15:38:08 +0700

    update on boarding

---
+++ app/src/main/java/personal/mine/bse/onBoarding/IntroActivity.java
@@ -0,0 +1,168 @@
+package personal.mine.bse.onBoarding;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.tabs.TabLayout;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import personal.mine.bse.MainActivity;
+import personal.mine.bse.R;
+
+public class IntroActivity extends AppCompatActivity {
+
+    private ViewPager screenPager;
+    IntroViewPagerAdapter introViewPagerAdapter;
+    TabLayout tabIndicator;
+    Button btnNext;
+    int position = 0;
+    Button btnGetStarted;
+    Animation btnAnim;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+
+        if (restorePrefData()) {
+
+            Intent mainActivity = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(mainActivity);
+            finish();
+
+
+        }
+
+
+
+        setContentView(R.layout.activity_intro);
+
+        getSupportActionBar().hide();
+
+        btnNext = findViewById(R.id.btn_next);
+        btnGetStarted = findViewById(R.id.btn_get_started);
+        tabIndicator = findViewById(R.id.tab_indicator);
+        btnAnim = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.button_animation);
+
+
+        final List<ScreenItem> mList = new ArrayList<>();
+        mList.add(new ScreenItem("Selamat Datang", "Nikmati Pengalaman Belajar Online", R.drawable.bse));
+        mList.add(new ScreenItem("Ribuan Konten Gratis", "Nikmati Pengalaman Belajar Online", R.drawable.bse));
+        mList.add(new ScreenItem("Belajar Berbagi", "Nikmati Pengalaman Belajar Online", R.drawable.bse));
+
+        screenPager = findViewById(R.id.screen_viewpager);
+        introViewPagerAdapter = new IntroViewPagerAdapter(this, mList);
+        screenPager.setAdapter(introViewPagerAdapter);
+
+        tabIndicator.setupWithViewPager(screenPager);
+
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                position = screenPager.getCurrentItem();
+                if (position < mList.size()) {
+
+                    position++;
+                    screenPager.setCurrentItem(position);
+
+
+                }
+
+                if (position == mList.size() - 1) {
+
+                    // TODO : show the GETSTARTED Buttton and hide the indicator and the next buttton
+
+
+                }
+
+            }
+        });
+
+        tabIndicator.addOnTabSelectedListener(new TabLayout.BaseOnTabSelectedListener() {
+
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+
+                if (tab.getPosition() == mList.size()-1) {
+
+                    loadLastScreen();
+                }
+
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+
+        btnGetStarted.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+                Intent mainActivity = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(mainActivity);
+
+
+                savePrefsData();
+                finish();
+
+            }
+        });
+
+
+    }
+
+    private boolean restorePrefData() {
+
+        SharedPreferences pref = getApplicationContext(). getSharedPreferences("myPrefs",MODE_PRIVATE);
+        Boolean isIntroActivityOpenBefore = pref.getBoolean("isIntroOpened",false);
+        return isIntroActivityOpenBefore;
+
+
+
+    }
+
+    private void savePrefsData() {
+
+        SharedPreferences pref = getApplicationContext().getSharedPreferences("myPrefs",MODE_PRIVATE);
+        SharedPreferences.Editor editor = pref.edit();
+        editor.putBoolean("isIntroOpened",true);
+        editor.commit();
+
+    }
+
+    private void loadLastScreen(){
+        btnNext.setVisibility(View.INVISIBLE);
+        btnGetStarted.setVisibility(View.VISIBLE);
+        tabIndicator.setVisibility(View.INVISIBLE);
+        // TODO : ADD an animation the getstarted button
+
+        btnGetStarted.setAnimation(btnAnim);
+    }
+}
