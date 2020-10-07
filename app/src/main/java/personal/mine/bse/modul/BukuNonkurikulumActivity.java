8aef1c0f75a51b00c8298ec24ca675f170344d49
mchmdarvan <muchamad.arvan27@gmail.com>
2020-10-07 18:09:20 +0700

    Edit Context

---
+++ app/src/main/java/personal/mine/bse/modul/BukuNonkurikulumActivity.java
@@ -0,0 +1,16 @@
+package personal.mine.bse.modul;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import personal.mine.bse.R;
+
+public class BukuNonkurikulumActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_buku_non_kurikulum);
+    }
+}
