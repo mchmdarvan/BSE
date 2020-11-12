6f364ea931f0bc43e5a71e1cffe715937271db78
mchmdarvan <muchamad.arvan27@gmail.com>
2020-11-12 15:07:21 +0700

    Login With API

--- app/src/main/java/personal/mine/bse/SplashActivity.java
+++ app/src/main/java/personal/mine/bse/SplashActivity.java
@@ -2,24 +2,111 @@ package personal.mine.bse;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
+import android.view.View;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import personal.mine.bse.onBoarding.IntroActivity;
 
+import static personal.mine.bse.utils.Config.loginUrl;
+
 public class SplashActivity extends Activity {
 
+    SharedPreferences sharedPreferences;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        sharedPreferences = getSharedPreferences("login", MODE_PRIVATE);
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
+                login();
                 Intent i = new Intent(SplashActivity.this, IntroActivity.class);
                 startActivity(i);
                 finish();
             }
         }, 5000);
+    }
+
+    private void login() {
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+
+        JSONObject postData = new JSONObject();
+        try {
+            postData.put("username", "bse_apps");
+            postData.put("password", "appsBSE2020");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        JsonObjectRequest stringRequest = new JsonObjectRequest(Request.Method.POST, loginUrl, postData,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        SharedPreferences.Editor editor = sharedPreferences.edit();
+                        try {
+                            Log.e("response2", response.getJSONObject("data").toString());
+                            Log.e("response", response.getJSONObject("data").getString("token"));
+                            editor.putString("token", response.getJSONObject("data").getString("token"));
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                        editor.apply();
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("Error:",error.getMessage());
+            }
+        });
+        requestQueue.add(stringRequest);
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            hideSystemUI();
+        }
+    }
+
+    private void hideSystemUI() {
+        // Enables regular immersive mode.
+        // For "lean back" mode, remove SYSTEM_UI_FLAG_IMMERSIVE.
+        // Or for "sticky immersive," replace it with SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_IMMERSIVE
+                        // Set the content to appear under the system bars so that the
+                        // content doesn't resize when the system bars hide and show.
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        // Hide the nav bar and status bar
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN);
+    }
+
+    // Shows the system bars by removing all the flags
+    // except for the ones that make the content appear under the system bars.
+    private void showSystemUI() {
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
     }
 }
---
+++ app/src/main/java/personal/mine/bse/utils/Config.java
@@ -0,0 +1,7 @@
+package personal.mine.bse.utils;
+
+public class Config {
+    public static String loginUrl = "http://apps.belajar.kemdikbud.go.id:3000/users/login";
+    public static String getContents = "http://apps.belajar.kemdikbud.go.id:3000/bse/contents";
+    public static String getDetail = "http://apps.belajar.kemdikbud.go.id:3000/bse/detail";
+}
